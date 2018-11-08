ecc02a8abf0eb3f9b27e6c29d025003bed09214d
xAlban <a.elezi68@gmail.com>
2018-11-08 17:04:05 +0100

    test commit

---
+++ desktop/src/com/to52/taquin/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.to52.taquin.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.to52.taquin.TaquinGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TaquinGame(), config);
+	}
+}
