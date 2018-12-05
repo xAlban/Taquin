b9a9a4e636682c9fd8d4895b13eb4942c12db269
xAlban <a.elezi68@gmail.com>
2018-12-05 13:52:42 +0100

    Ajout de boutton de difficulté + resolution du puzzle

--- core/src/com/to52/taquin/Scenes/PlayScreen.java
+++ core/src/com/to52/taquin/Scenes/PlayScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -29,10 +30,14 @@ public class PlayScreen implements Screen {
     public ArrayList<Tuile> listeTuile = new ArrayList<Tuile>();
     public ArrayList<Tuile> solution = new ArrayList<Tuile>();
     private Stage stage;
-    private int colonne = 5;
-    private int ligne = 5;
+    private int colonne = 3;
+    private int ligne = 3;
 
     private TextButton melanger;
+    private TextButton grille3x3;
+    private TextButton grille4x4;
+    private TextButton grille5x5;
+    private TextButton rejouer;
 
     public PlayScreen(TaquinGame game){
         this.game = game;
@@ -44,20 +49,7 @@ public class PlayScreen implements Screen {
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
         generatePuzzle(ligne,colonne);
-
-        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
-        style.font = new BitmapFont();
-        melanger = new TextButton("MELANGER",style);
-        melanger.setPosition(40,690);
-        melanger.setHeight(20);
-        melanger.setWidth(20);
-        melanger.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y){
-                shuffleAndPlace(listeTuile, ligne, colonne);
-            }
-        });
-        stage.addActor(melanger);
+        createButtons();
     }
     @Override
     public void show() {
@@ -68,19 +60,31 @@ public class PlayScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        //if(checkFinish()){
-        //    System.out.println(checkFinish());
-        //}
+        if(checkFinish()){
+            //System.out.println(checkFinish());
+            removeTuile(solution);
+            removeActors();
+            rejouer.setVisible(true);
+            stage.act();
+            stage.getBatch().begin();
+            stage.getBatch().draw(background,0,0);
+            stage.getBatch().end();
+            stage.addActor(rejouer);
+            stage.draw();
 
-        camera.update();
-        stage.act();
-        stage.getBatch().begin();
-        stage.getBatch().draw(background,0,0);
-        stage.getBatch().draw(testTexture,
-                            (float) (testTexture.getWidth()*1.4),
-                            (gamePort.getWorldHeight()-testTexture.getHeight()) / 2);
-        stage.getBatch().end();
-        stage.draw();
+        }else{
+            camera.update();
+            stage.act();
+            stage.getBatch().begin();
+            stage.getBatch().draw(background,0,0);
+            stage.getBatch().draw(testTexture,
+                    (float) (testTexture.getWidth()*1.4),
+                    (gamePort.getWorldHeight()-testTexture.getHeight()) / 2);
+            stage.getBatch().end();
+            stage.draw();
+        }
+
+
 }
 
     @Override
@@ -156,21 +160,115 @@ public class PlayScreen implements Screen {
     }
 
     private boolean checkFinish(){
-        boolean finish = false;
-        int i = 0;
+        boolean finish = true;
+        int i = ligne*(ligne-1);
+        int col = 0;
         for (Tuile t : listeTuile){
             if (t.solution != solution.get(i).solution){
                 finish = false;
-                break;
-            }else {
-                finish = true;
             }
-            i++;
+            if(i< ligne){
+                col++;
+                i = ligne*(ligne-1) + col;
+            }else{
+                i -= ligne;
+            }
         }
         return finish;
     }
 
     public StretchViewport getGamePort(){
         return this.gamePort;
+    }
+
+    private void createButtons(){
+        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+        style.font = new BitmapFont();
+        melanger = new TextButton("MELANGER",style);
+        melanger.setPosition(40,690);
+        melanger.setHeight(20);
+        melanger.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                shuffleAndPlace(listeTuile, ligne, colonne);
+            }
+        });
+
+        grille3x3 = new TextButton("GRILLE 3X3",style);
+        grille3x3.setPosition(melanger.getX() + melanger.getWidth() + 100,690);
+        grille3x3.setHeight(20);
+        grille3x3.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                removeTuile(solution);
+                solution = new ArrayList<Tuile>();
+                ligne = 3;
+                colonne = 3;
+                generatePuzzle(ligne,colonne);
+            }
+        });
+
+
+        grille4x4 = new TextButton("GRILLE 4X4",style);
+        grille4x4.setPosition(grille3x3.getX()+grille3x3.getWidth() + 100,690);
+        grille4x4.setHeight(20);
+        grille4x4.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                removeTuile(solution);
+                solution = new ArrayList<Tuile>();
+                ligne = 4;
+                colonne = 4;
+                generatePuzzle(ligne,colonne);
+            }
+        });
+
+        grille5x5 = new TextButton("GRILLE 5X5",style);
+        grille5x5.setPosition(grille4x4.getX()+grille4x4.getWidth() + 100,690);
+        grille5x5.setHeight(20);
+        grille5x5.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+                removeTuile(solution);
+                solution = new ArrayList<Tuile>();
+                ligne = 5;
+                colonne = 5;
+                generatePuzzle(ligne,colonne);
+            }
+        });
+
+        rejouer = new TextButton("REJOUER",style);
+        rejouer.setHeight(20);
+        rejouer.setPosition(gamePort.getWorldWidth()/2 - rejouer.getWidth()/2,gamePort.getWorldHeight()/2 - rejouer.getHeight()/2);
+        rejouer.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y){
+
+                solution = new ArrayList<Tuile>();
+                ligne = 5;
+                colonne = 5;
+                generatePuzzle(ligne,colonne);
+                createButtons();
+            }
+        });
+        rejouer.setVisible(false);
+
+        stage.addActor(grille3x3);
+        stage.addActor(grille4x4);
+        stage.addActor(grille5x5);
+        stage.addActor(melanger);
+        stage.addActor(rejouer);
+    }
+
+    private void removeTuile(ArrayList<Tuile> liste){
+        for (Tuile t : liste){
+            t.remove();
+        }
+    }
+
+    private void removeActors(){
+        for(Actor a : stage.getActors()){
+            a.remove();
+        }
     }
 }
